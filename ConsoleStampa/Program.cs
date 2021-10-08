3f50dd29164866a10076e675468fd152f8aa1e13
AbderazakErraknaoui <91722623+AbderazakErraknaoui@users.noreply.github.com>
2021-10-08 09:25:22 +0200

    Update Program.cs

--- ConsoleStampa/Program.cs
+++ ConsoleStampa/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleStampa
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
 
             Console.ReadKey();
         }
